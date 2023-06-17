6caa6a12156255f5a5d9c5c864bdb0277416c6d8
MariaCamarillo <mdelosangeles.camarillo@innovant.us>
2023-06-17 14:45:22 -0300

    integracion de spotify en proceso

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+if __name__ == 'main':
+    app.run(debug=True, port=5000)
+
 @app.route("/")
-def hello_world():
-    return "Hello, World!"
+def ping():
+    return jsonify({"message": "pong"})
+
+@app.route("/login", methods=['POST'])
+def login():
+    return jsonify({"message": "login"})
+
+@app.route("/upload/<varname>", methods=['POST'])
+def uploadPodcast():
+    return jsonify({"message": "login"})
+# @app.route("/")
+# def hello_world():
+#     return "Hello, World!"
