55fcb873767639ae26bb37a775abf282b05185a1
Nelson1411 <drosswerever@gmail.com>
2023-06-22 00:54:13 -0400

    update CRUD

--- app.py
+++ app.py
@@ -1,21 +1,18 @@
-from flask import Flask, request, render_template, redirect, url_for, flash
+from flask import Flask, request, render_template, redirect, url_for, flash, jsonify
 import gspread
 
 app = Flask(__name__)
 
+#funcion para abrir la hoja
 def open_spreadsheet():
     client = gspread.service_account(filename="./secret/credentials.json")
     sheet = client.open("podcast-2023").sheet1
     return sheet
 
-def send_user_new(name, email):
+#funcion para comparar si existe un usuario comparando si estan en la misma fila
+def confirm_user(old_name, old_email):
     sheet = open_spreadsheet()
-    row = [name, email]
-    sheet.append_row(row)
-
-def confirm_user(old_nombre, old_email):
-    sheet = open_spreadsheet()
-    value1 = sheet.find(old_nombre)
+    value1 = sheet.find(old_name)
     value2 = sheet.find(old_email)
     row_value1 = value1.row
     row_value2 = value2.row
@@ -24,71 +21,92 @@ def confirm_user(old_nombre, old_email):
     else:
         return print("El usuario no existe")
 
-def update_user(new_name, new_email, old_name, old_email):
+#funcion para crear usuario
+def send_user_new(name, email, phone):
+    sheet = open_spreadsheet()
+    row = [name, email, phone]
+    sheet.append_row(row)
+
+#funcion para actualizar usuario
+def update_user(new_name, new_email, new_phone , old_name, old_email):
     sheet = open_spreadsheet()
     row_value = confirm_user(old_name, old_email)
     if row_value:
         sheet.update_cell(row_value, 1, new_name)
         sheet.update_cell(row_value, 2, new_email)
+        sheet.update_cell(row_value, 3, new_phone)
         
     else:
         print("El usuario ya existe")
 
+#funcion para eliminar usuario
 def delete_user(name, email):
     sheet = open_spreadsheet()
     row_value = confirm_user(name, email)
     if row_value:
-        sheet.delete_row(row_value)
+        sheet.delete_rows(row_value)
 
-@app.route('/')
-
-@app.route('/create_user', methods = ['GET', 'POST'])
+#rutas
+@app.route('/create_user', methods = ['POST'])
 def create_user():
     if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
-        if not name:
-            print('name is required')
-        elif not email:
-            print('email is required')
+        name = request.args.get('name')
+        email = request.args.get('email')
+        phone = request.args.get('phone')
+        if name and email and phone:
+            send_user_new(name, email, phone)
         else:
-            send_user_new(name, email)
-            return redirect(url_for('get_sheet'))
-    return render_template("form_create_user.html")
+            name = request.form['name']
+            email = request.form['email']
+            if name and email and phone:
+                send_user_new(name, email, phone)
+            else:
+                return jsonify({"message": "error"}), 400
+    return jsonify({"message": "Usuario creado", "data": {"new_user": {"name": name, "email": email, "phone": phone}}}), 200
 
-@app.route('/get_sheet')
+@app.route('/get_users', methods = ['GET'])
 def get_sheet():
     sheet = open_spreadsheet()
     data = sheet.get_all_records()
-    return str(data)
+    return jsonify({"message": "Solicitud exitosa", "data": {"users": data}}), 200
 
-@app.route('/update_user', methods = ['GET', 'POST'])
+@app.route('/update_user', methods = ['PUT'])
 def form_update_user():
-    if request.method == 'POST':
-        old_name = request.form['old_name']
-        old_email = request.form['old_email']
-        new_name = request.form['new_name']
-        new_email = request.form['new_email']
-
-        if not old_name or not old_email:
-            print('name or email is required')
+    if request.method == 'PUT':
+        old_name = request.args.get('old_name')
+        old_email = request.args.get('old_email')
+        new_name = request.args.get('new_name')
+        new_email = request.args.get('new_email')
+        new_phone = request.args.get('new_phone')
+        if old_name and old_email and new_phone and new_name and new_email:
+            update_user(new_name, new_email, new_phone, old_name, old_email)
         else:
-            update_user(new_name, new_email, old_name, old_email)
-            return redirect(url_for('get_sheet'))
-    return render_template("form_update_user.html")
+            old_name = request.form['old_name']
+            old_email = request.form['old_email']
+            new_name = request.form['new_name']
+            new_email = request.form['new_email']
+            new_phone = request.form['new_phone']
+            if old_name and old_email and new_name and new_email:
+                update_user(new_name, new_email, new_phone, old_name, old_email)
+            else:
+                return jsonify({"message": "error"}), 400
+    return jsonify({"message": "Usuario actualizado", "data": {"update_user": {"name": new_name, "email": new_email, "phone": new_phone}}}), 200
 
-@app.route('/delete_user', methods = ['GET', 'POST'])
+@app.route('/delete_user', methods = ['DELETE'])
 def form_delete_user():
-    if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
-
-        if not name or not email:
-            print('name or email is required')
-        else:
+    if request.method == 'DELETE':
+        name = request.args.get('name')
+        email = request.args.get('email')
+        if name and email:
             delete_user(name, email)
-            return redirect(url_for('get_sheet'))
-    return render_template("form_delete_user.html")
+        else:
+            name = request.form['name']
+            email = request.form['email']
+            if name and email:
+                delete_user(name, email)
+            else:
+                return jsonify({"message": "error"}), 400
+    return jsonify({"message": "Usuario eliminado"}), 200
 
 if __name__ == '__main__':
     app.run()
