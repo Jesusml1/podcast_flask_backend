bb25edae5a5a11ad8105d9ec2e6a79a6cff630d7
jesus <jesus_lopez1997@hotmail.com>
2023-06-27 14:38:39 -0400

    feat: spotify blueprint

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from api.google_sheets.google_sheets_api import google_sheets_bp
+from api.spotify.spotify_api import spotify_bp
 
 app = Flask(__name__)
 
 app.register_blueprint(google_sheets_bp)
+app.register_blueprint(spotify_bp)
+
+app.config['SESSION_COOKIE_NAME'] = 'Spotify Cookie'
+app.secret_key = 'spotifyappmariiacao'
 
 if __name__ == '__main__':
     app.run()
